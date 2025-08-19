e4b55db1c28e25f46a964003ea86e05e85f6f77f
Elvinkyungu <elvinkyungu.75@gmail.com>
2025-08-19 18:19:10 +0100

    Added support for Tailwind CSS and removed obsolete PostCSS configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
@@ -10,6 +11,11 @@ export default defineNuxtConfig({
     '@nuxt/test-utils',
     '@nuxt/ui',
     '@nuxt/scripts',
-    // '@nuxtjs/tailwindcss'
-  ]
+  ],
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 })
---
+++ postcss.config.mjs
@@ -0,0 +1,7 @@
+    // postcss.config.mjs
+    export default {
+      plugins: {
+        "@tailwindcss/postcss": {}, // Use the dedicated PostCSS plugin
+        // Other PostCSS plugins if you have them
+      },
+    };
